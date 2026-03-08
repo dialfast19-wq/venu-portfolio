e5e2723c881c840cb7e621221b95c797129d02dc
EC2 User <ec2-user@example.com>
2026-03-08 17:03:47 +0000

    Add .next to gitignore and remove from repo

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/ec2-user/workspace/my-workspace/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2","static/media/eaead17c7dbfcd5d-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
